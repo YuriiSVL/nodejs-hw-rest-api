8436e0bf644f85ba74f61f1eca17a68e7c89599a
Yurii Syvolapenko <svl.yurii@gmail.com>
2023-07-20 20:33:36 +0300

    refactoring (add controllers-folder, schemas-folder, wrapper middleware)

---
+++ controllers/contacts.js
@@ -0,0 +1,69 @@
+// const Joi = require("joi");
+
+// const addSchema = Joi.object({
+//   name: Joi.string().required(),
+//   email: Joi.string().required(),
+//   phone: Joi.string().required(),
+// });
+
+// const addSchema = require("../schemas/contacts");
+
+const { HttpError, ctrlWrapper } = require("../helpers");
+
+const contacts = require("../models/contacts");
+
+const getAll = async (req, res) => {
+  const result = await contacts.listContacts();
+  res.json(result);
+};
+
+const getById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.getContactById(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json(result);
+};
+
+const add = async (req, res) => {
+  //   const { error } = addSchema.validate(req.body);
+  //   if (error) {
+  //     throw HttpError(400, "missing required name field");
+  //   }
+  const result = await contacts.addContact(req.body);
+  res.status(201).json(result);
+};
+
+const deleteContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.removeContact(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json({ message: "contact deleted" });
+};
+
+const update = async (req, res) => {
+  //   const { error } = addSchema.validate(req.body);
+
+  //   if (error) {
+  //     throw HttpError(400, "missing fields");
+  //   }
+  const { contactId } = req.params;
+  const result = await contacts.updateContact(contactId, req.body);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json(result);
+
+  return result;
+};
+
+module.exports = {
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  add: ctrlWrapper(add),
+  deleteContact: ctrlWrapper(deleteContact),
+  update: ctrlWrapper(update),
+};
